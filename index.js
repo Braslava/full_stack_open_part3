3645919274249bd9c6406ef3d1c5d88b12c396e3
Mara Braslava <m.braslava@gmail.com>
2022-06-06 09:08:19 +0300

    3.2

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 3001;
 const persons = require("./data");
 
+const date = new Date();
+
+//console.log(persons);
 app.get("/api/persons", (req, res) => {
     res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+    res.send(
+        `<p>The phonebook has ${persons.length} entries</p><p>${date}</p>`
+    );
 });
 
 app.listen(port, () => {
